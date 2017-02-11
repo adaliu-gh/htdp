dd1dcb84d9aebef8182a82cb653d8ddfb82d5ec0
adaliuada <twostepsfrom_hell@outlook.com>
2017-02-11 18:58:46 +0800

    add features to 223

--- 8-13 Arbitrarily Large Data/12-220-223.rkt
+++ 8-13 Arbitrarily Large Data/12-220-223.rkt
@@ -8,7 +8,7 @@
 
 ;; constants
 
-(define SPEED 0.2) ; of the dropping of blocks
+(define SPEED 0.01) ; of the dropping of blocks
 (define WIDTH 10) ; # of blocks, horizontally
 (define HEIGHT (* WIDTH 3/2))
 (define SIZE 10) ; blocks are squares
@@ -20,11 +20,18 @@
 (define MT (empty-scene SCENE-SIZE
                         (* SIZE HEIGHT)))
 
-
+; Number -> List-of-numbers
+(check-expect (generate-rows 3) (list 2 1 0))
+(define (generate-rows n)
+  (cond
+    [(zero? n) '()]
+    [else (cons (- n 1)
+                (generate-rows (- n 1)))]))
+(define ROWS (generate-rows HEIGHT))
 ;; data definitions
-(define-struct tetris [block landscape])
+(define-struct tetris [block landscape score])
 (define-struct block [x y])
-
+(define-struct row [index many])
 ;  A Tetris is a structure:
 ; (make-tetris Block Landscape)
 ; A Landscape is one of:
@@ -39,6 +46,7 @@
 ; (* y SIZE) pixels from the top;
 ; (make-tetris b0 (list b1 b2 ...)) means b0 is the
 ; dropping block, while b1, b2, and ... are resting
+
 
 ; ===================
 ; 220
@@ -73,7 +81,8 @@
 ; Any -> Tetris
 (define (tetris-main any)
   (big-bang (make-tetris (block-generate WIDTH)
-                         landscape0)
+                         landscape0
+                         0)
             [stop-when touch-top? last-image]
             [on-key tetris-control]
             [on-tick tetris-tock SPEED]
@@ -81,20 +90,24 @@
 
 ; Tetris -> Tetris
 (define (tetris-tock t)
-  (if (land? t)
-      (tetris-update t)
-      (tetris-dropping t)))
+  (cond
+    [(land? t)(tetris-update t)]
+    [(has-full-lines? t) (remove-lines-in-tetris t)]
+    [else (tetris-dropping t)]))
+
 
 ; Tetris -> Tetris
 (define (tetris-update t)
   (make-tetris (block-generate WIDTH)
                (cons (tetris-block t)
-                     (tetris-landscape t))))
+                     (tetris-landscape t))
+               (tetris-score t)))
 
 ; Tetris -> Tetris
 (define (tetris-dropping t)
   (make-tetris (block-dropping (tetris-block t))
-               (tetris-landscape t)))
+               (tetris-landscape t)
+               (tetris-score t)))
 
 ; Tetris -> Boolean
 (define (land? t)
@@ -118,17 +131,28 @@
 
 ; Tetris KeyEvent -> Tetris
 (define (tetris-control t k)
-  (if (touch-wall? t k)
-      t
-      (make-tetris (block-slide (tetris-block t) k)
-                                (tetris-landscape t))))
+  (cond
+    [(member? k (list "left" "right"))
+      (control-block t k)]
+    [else t]))
 
-; Tetris KeyEvent -> Tetris
-(define (touch-wall? t k)
+; Tetris String -> Tetris
+(define (control-block t k)
+  (cond
+    [(touch-side? t k) t]
+    [else (make-tetris (block-slide (tetris-block t) k)
+                       (tetris-landscape t)
+                       (tetris-score t))]))
+
+
+; Tetris String-> Boolean
+(define (touch-side? t k)
   (or (member? (block-slide (tetris-block t) k)
                (tetris-landscape t))
-      (member? (block-x (tetris-block t))
-               (list 0 (- WIDTH 1)))))
+      (= (block-x (tetris-block t))
+         (if (string=? k "left")
+             0
+             (- WIDTH 1)))))
 
 ; Block String -> Block
 (define (block-slide b d)
@@ -172,11 +196,70 @@
 (define (last-image t)
   (place-image (text (number->string (get-score t))
                       (* SIZE 2) "blue")
-               (/ (image-width MT) 2)
-               (/ (image-height MT) 2)
+               2  2
                (tetris-render t)))
 
 ; Tetris -> Number
 (define (get-score t)
-  (length (tetris-landscape t)))
+  (tetris-score t))
 
+; ====================
+; my own features - removing full lines
+; ====================
+
+; Tetris -> Boolean
+(define (has-full-lines? t)
+  (> (length (final-full-lines t)) 0))
+
+; Tetris -> List-of-numbers
+(define (final-full-lines t)
+  (full-lines (all-lines (tetris-landscape t))))
+
+; List-of-rows -> List-of-numbers
+(check-expect (full-lines (list (make-row 8 10) (make-row 7 3)))
+              (list 8))
+(define (full-lines r)
+  (cond
+    [(empty? r) '()]
+    [else (if (= (row-many (first r)) WIDTH)
+              (cons (row-index (first r))
+                    (full-lines (rest r)))
+              (full-lines (rest r)))]))
+
+; Landscape -> List-of-rows
+(define (all-lines l)
+  (get-all-lines l ROWS))
+
+; Landscape List-of-numbers -> List-of-rows
+(define (get-all-lines l r)
+  (cond
+    [(empty? r) '()]
+    [else (cons (make-row (first r )
+                          (count-a-line l (first r)))
+                (get-all-lines l (rest r)))]))
+
+; Landscape Number -> Number
+(check-expect (count-a-line (list (make-block 2 3)) 3) 1)
+(define (count-a-line l n)
+  (cond
+    [(empty? l) 0]
+    [else (if (= n (block-y (first l)))
+              (+ 1 (count-a-line (rest l) n))
+              (count-a-line (rest l) n))]))
+
+; Tetris -> Tetris
+(define (remove-lines-in-tetris t)
+  (make-tetris (tetris-block t)
+               (remove-lines (tetris-landscape t)
+                             (final-full-lines t))
+               (+ (length (final-full-lines t)) (tetris-score t))))
+
+; Landscape List-of-numbers -> Landscape
+(define (remove-lines l n)
+  (cond
+    [(empty? l) '()]
+    [else (if (member? (block-y (first l)) n)
+              (remove-lines (rest l) n)
+              (cons (make-block (block-x (first l))
+                                (+ 1 (block-y (first l))))
+                    (remove-lines (rest l) n)))]))
