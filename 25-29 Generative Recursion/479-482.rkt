6d5cacf5a374c3ac201e2c596986160eeb403de5
adaliu-gh <adaliu.gh@outlook.com>
2017-03-17 16:35:22 +0800

    add 482

--- 25-29 Generative Recursion/479-482.rkt
+++ 25-29 Generative Recursion/479-482.rkt
@@ -47,7 +47,7 @@
 ;; render an image of an n by n chess board with image i placed according to l
 
 (define (render-queens n l i)
-  (place-queens l i (chess-board n)))
+  (render-queens/list l i (chess-board n)))
 
 ;; Number -> Image
 ;; create an n by n chess board
@@ -67,12 +67,12 @@
 
 ;; [List-of QP] Image Image -> Image
 ;; places queens according to l upto the chess-board
-(define (place-queens l i c)
+(define (render-queens/list l i c)
   (local ((define (real-coordinate x)
             (+ (/ CELL-WIDTH 2) (* CELL-WIDTH x))))
     (cond
      [(empty? l) c]
-     [else (place-queens (rest l)
+     [else (render-queens/list (rest l)
                          i
                          (place-image i
                                       (real-coordinate (posn-x (first l)))
@@ -113,3 +113,72 @@
     (lambda (solution)
       (and (= n (length solution))
            (not-threatening? solution))) ))
+
+;;===========================
+;; 482
+
+;; a Board is a structure:
+(define-struct board [checked open])
+;; interpretation (make-board [List-of QP] [List-of Posn])
+;; where checked is made up of all queens on the board and
+;; open is made up of all cells that are still open
+
+;; N -> Board
+;; creates the initial n by n board
+(define (board0 n)
+  (local ((define list-of-n (build-list n (lambda (n) n)))
+          (define (create-nth-row n)
+            (map (lambda (x) (make-posn n x)) list-of-n)))
+    (make-board '() (foldr (lambda (x y)
+                             (append (create-nth-row x) y))
+                           '()
+                           list-of-n) )))
+
+;; Board QP -> Board
+;; places a queen at qp on a-board
+(check-expect (add-queen (board0 2) (make-posn 0 0))
+              (make-board (list (make-posn 0 0)) (list (make-posn 0 1)
+                                                       (make-posn 1 0)
+                                                       (make-posn 1 1)) ))
+(define (add-queen a-board qp)
+  (make-board (cons qp (board-checked a-board))
+              (remove qp (board-open a-board))))
+
+
+;; Board -> [List-of QP]
+;; finds spots where it is still safe to place a queen
+(define (find-open-spots a-board)
+  (local ((define checked (board-checked a-board))
+          (define open (board-open a-board))
+
+          (define (not-threatening? p)
+            (andmap (lambda (x) (not (threatening? p x))) checked)))
+    (filter not-threatening? open)))
+
+;; Board N -> [Maybe [List-of QP]]
+;; place n queens on board; otherwise, return #false
+(check-satisfied (place-queens (board0 8) 8)
+                 (n-queen-solution? 8))
+(define (place-queens a-board n)
+  (cond
+   [(zero? n) '()]
+   [else (local ((define candidates (find-open-spots a-board))
+                 (define potential-result (place-queens/list a-board candidates n)))
+           (cond
+            [(boolean? potential-result) #false]
+            [else potential-result]))]))
+
+
+;; Board [List-of QP] Number -> [Maybe [List-of QP]]
+(define (place-queens/list a-board candidates n)
+  (cond
+   [(empty? candidates) #false]
+   [else (local ((define updated-board
+                   (add-queen a-board (first candidates)))
+                 (define potential-result
+                   (place-queens updated-board (sub1 n))))
+           (cond
+            [(boolean? potential-result)
+             (place-queens/list a-board (rest candidates) n)]
+            [else (cons (first candidates)
+                        potential-result)]))]))
